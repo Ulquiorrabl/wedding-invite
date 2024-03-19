ebde2c2e4a85e2aac2b11ca1b2115296053ec24e
Vladislav Eremo <ulqrbl@gmail.com>
2024-03-19 21:58:50 +0300

    fix iva text + add plan 15:30

--- src/App.jsx
+++ src/App.jsx
@@ -62,11 +62,12 @@ function App() {
           <span className='exo-text header'>Дата</span>
           <img className='calendar-image photo-box-shadow' src={calendar}></img>
           <span className='exo-text header'>Место проведения</span>
-          <a href='https://yandex.by/maps/org/usadba_iva_park/153258836002/?ll=23.874049%2C53.562482&z=17' className='link-place'><img className='svg' src={mapPoint}></img><span className='exo-text header2 link-text place-text'>{'Усадьба "Ива Парк", 13км от г. Гродно'}</span></a>
+          <a href='https://yandex.by/maps/org/usadba_iva_park/153258836002/?ll=23.874049%2C53.562482&z=17' className='link-place'><img className='svg' src={mapPoint}></img><span className='exo-text header2 link-text place-text'>{'Усадьба "Ива Парк", 5км от г. Гродно'}</span></a>
           <img className='place-image photo-box-shadow' src={iva1}></img>
           <span className='exo-text header'>Программа</span>
           <ul>
             <li className='b1'>15:00 - Сбор гостей около <a href="https://yandex.by/maps/org/hrodnenska_oblasna_filarmoniia/127044541592/?ll=23.822186%2C53.668056&z=17.05"><span className='exo-text b1 link-text place-text'>Гродненская областная филармония</span><img className='svg' src={mapPoint}></img></a> для трансфера к месту проведения мероприятия</li>
+            <li className='b1'>15:30 - Фуршет</li>
             <li className='b1'>16:00 - Церемония бракосочетания</li>
             <li className='b1'>17:00 - Праздничный банкет</li>
           </ul>
